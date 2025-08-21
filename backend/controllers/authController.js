d8004ebf577e91c7b6c644aa268a88892bb286f6
Samiksha Jain <samikshaj7371@gmail.com>
2025-08-21 23:31:32 +0530

    Initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,74 @@
+import genToken from "../config/token.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+export const signUp = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    const checkByUsername = await User.findOne({ userName });
+    if (checkByUsername) {
+      return res.status(400).json({ message: "UserName already exists!" });
+    }
+    const checkByEmail = await User.findOne({ email });
+    if (checkByEmail) {
+      return res.status(400).json({ message: "Email already exists!" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 6 characters" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+    const token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "None",
+      secure: false,
+    });
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `signup error ${error}` });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "user does not exists!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+    const token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "None",
+      secure: false,
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `login error ${error}` });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "log out successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: `logout error ${error}` });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRouter from "./routes/userRoutes.js";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => {
+  connectDb();
+  console.log("server started");
+});
