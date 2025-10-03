866f67b5e20f7b68a24b9f6db395c46204ab1c67
Samiksha Jain <samikshaj7371@gmail.com>
2025-10-03 21:54:00 +0530

    Create message and conversation model

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [{ type: String }],
+  },
+  { timestamps: true }
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
