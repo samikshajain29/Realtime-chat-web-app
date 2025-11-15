8f2642268ae6f9ea20dd94a74473724b68281a1a
Samiksha Jain <samikshaj7371@gmail.com>
2025-11-15 11:12:59 +0530

    Update cookie settings for token in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,8 +29,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 10 * 365 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(201).json(user);
   } catch (error) {
@@ -55,8 +55,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 10 * 365 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(200).json(user);
   } catch (error) {
