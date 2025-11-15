010d661ca2a4de6c8020c92ce685d8c15db3ec59
Samiksha Jain <samikshaj7371@gmail.com>
2025-11-15 11:10:26 +0530

    Change CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtimechatapp-4qhh.onrender.com",
     credentials: true,
   })
 );
